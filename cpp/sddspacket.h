684fea7636c127cd89e5257fdf0cc505c3594eb2
Kipp Bowen <jkb@axiosengineering.com>
2016-08-03 18:08:43 -0400

    Add REDHAWK headers.

--- cpp/sddspacket.h
+++ cpp/sddspacket.h
@@ -1,3 +1,22 @@
+/*
+ * This file is protected by Copyright. Please refer to the COPYRIGHT file
+ * distributed with this source distribution.
+ *
+ * This file is part of REDHAWK rh.SourceSDDS.
+ *
+ * REDHAWK rh.SourceSDDS is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * REDHAWK rh.SourceSDDS is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
 // Class that handles a SDDS packet, assuming little-endian arch (Tru64, x86 Linux)
 // Defines a swap8() function, ala htonll()
 // Uses class XMTime for XMidas time representation
